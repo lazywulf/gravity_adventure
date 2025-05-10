00c92fbd182b6f9aacd341c3f8da1f61b079493f
Illusionary-Night <0s5h1a2s@gmail.com>
2025-05-10 16:24:50 +0800

    volcano

--- Assets/Scripts/Gravity/GravPad.cs
+++ Assets/Scripts/Gravity/GravPad.cs
@@ -6,8 +6,17 @@ public class GravPad : GravForce
 {
 	[SerializeField] Vector2 gravity = new();
 	[SerializeField] ForceMode2D mode = ForceMode2D.Force;
-
-	public override void ApplyGravitationalForce(Rigidbody2D rb) {
+	[SerializeField] bool UseAutoCheeseGravity=false;
+    private void Start()
+    {
+        if (UseAutoCheeseGravity) ChooseDirection();
+    }
+    public override void ApplyGravitationalForce(Rigidbody2D rb) {
 		rb.AddForce(gravity, mode);
 	}
+	public void ChooseDirection()
+	{
+        float rad = (transform.eulerAngles.z+90) * Mathf.Deg2Rad;
+        gravity = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
+    }
 }
